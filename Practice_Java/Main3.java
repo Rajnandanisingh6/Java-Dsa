5310b2dc3bfb61e25acf17503840450703eca20e
Rajnandanisingh6 <rajnandanisingh07610761@gmail.com>
2025-09-07 13:18:07 +0530

    casting

--- Practice_Java/Main3.java
+++ Practice_Java/Main3.java
@@ -1,9 +1,95 @@
 package Practice_Java;
 
+import java.util.Arrays;
+
 public class Main3 {
     public static void main(String[] args) {
         //Arrays
-        
+      /*   
+        int physics =97;
+        int chem=89;
+        int eng =87;
+        int [] marks=new int[3];
+        marks[0]=97;
+        marks[1]=89;
+        marks[2]=87;
+       // System.out.println(marks[0]);
+       //length
+
+     //    System.out.println(marks.length);
+
+       //sort
+       System.out.println(marks[0]);
+       Arrays.sort(marks);
+       System.out.println(marks[0]);
+
+       */
+    //   int [] marks ={98,90,97};
+    //   int [][] finalMarks={{98,90,97},{95,93,92}};
+    //   System.out.println(finalMarks[0][0]);
+
+    //Casting
+    //Implicit Casting
+    // double price =100.00;
+    // double finalPrice = price +18;
+
+    // System.out.println(finalPrice);
+
+    //--------------------
+    //Explicit Casting
+    // int p=100;
+    // int fp =p + (int)18.0;
+    // System.out.println(fp);
+
+    //---------------------------
+    //Constants
+    // int age =30;
+    
+    // final float PI =3.14F;
+
+    //Operators
+    //Arithmetic Operators
+    //Assignments Operators
+    //Logical Operators
+    //Comparision Operators
+
+    //Arithmetic Operators
+    // int a=3;
+    // int b=5;
+    // int sum=a+b;
+    // System.out.println(sum);
+
+    // double a=5;
+    // double b=3;
+    // int diff=a-b;
+    // int mul =a*b;
+    // double div=a / b;
+    // double modulo=a % b;
+
+    // System.out.println(diff);
+    // System.out.println(mul);
+    // System.out.println(modulo);
+
+
+    //Assignments Operators
+    // int numb=1;
+    // numb =numb +1;
+   // numb++; uniary operator
+//    System.out.println(++numb);//2
+//    System.out.println(numb);//2
+
+   //Math Class
+   //5,6
+   System.out.println(Math.max(5,6));
+   System.out.println(Math.min(5,6));
+   //random 0.0 se 1.0 tak 
+   System.out.println(Math.random());
+   System.out.println((int)(Math.random()*100));
+
+
+
+
+
     }
     
 }
